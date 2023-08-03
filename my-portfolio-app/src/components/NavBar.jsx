89154c53adc13ba4b3cd244e30d7c4940ff00d01
bTharsis <eunoia709@gmail.com>
2023-08-02 21:55:13 -0600

    Creacion de esqueleto del portafolio con iconos de SM

--- my-portfolio-app/src/components/NavBar.jsx
+++ my-portfolio-app/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
     return (
         <>
-            <nav className="text-white border-2 rounded-lg w-1/2 row-start-3 row-end-5 col-start-1 col-end-2 flex flex-col justify-items-center gap-3 items-center ml-8">
+            <nav className="text-white rounded-lg w-1/2 row-start-3 row-end-5 col-start-1 col-end-2 flex flex-col justify-items-center gap-3 items-center ml-8">
                 <h2 className="font-bold text-2xl">Nav Bar</h2>
                 <ul className="font-medium font-mono flex flex-col justify-between gap-4">
                     <li>
--- my-portfolio-app/src/components/PCard.jsx
+++ my-portfolio-app/src/components/PCard.jsx
@@ -3,7 +3,7 @@ function PCard() {
     return (
         <>
             <div className=" row-start-3 row-end-5 col-start-2 col-end-6">
-                <div className=" border-2 grid grid-flow-row grid-rows-6 grid-cols-6">
+                <div className=" grid grid-flow-row grid-rows-6 grid-cols-6">
                     <div className="font-serif row-start-1 row-end-3 col-start-2 col-end-6 flex justify-center">
                         <h1 className="text-4xl text-zinc-100 font-bold pt-4 r">
                             Brandon Hernandez Montes
@@ -19,15 +19,15 @@ function PCard() {
                             voluptatum non eaque!
                         </p>
                     </div>
-                    <div className="border-2 row-start-6 row-end-7 col-start-2 col-end-6 text-slate-50">
+                    <div className="row-start-6 row-end-7 col-start-2 col-end-6 text-slate-50">
                         <ul className="flex justify-evenly mt-2">
-                            <li>
+                            <li className="text-4xl cursor-pointer">
                                 <BsGithub />
                             </li>
-                            <li>
+                            <li className="text-4xl cursor-pointer">
                                 <BsLinkedin />
                             </li>
-                            <li>
+                            <li className="text-4xl cursor-pointer">
                                 <BsInstagram />
                             </li>
                         </ul>
